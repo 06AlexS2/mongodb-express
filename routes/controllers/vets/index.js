4acd41448bf4295e6f593c2f3c766c26a3ef4373
06AlexS2 <sanchez.alejandro0603@gmail.com>
2023-03-20 17:08:55 -0600

    updated the vets handler, not quite express but functional

--- routes/controllers/vets/index.js
+++ routes/controllers/vets/index.js
@@ -1,33 +1,93 @@
 const router = require("express").Router();
+const Vet = require("./schema");
 
-const { create, list, getOne, update, erase } = require("../generics");
+const { /*create, list, */ getOne, update, erase } = require("../generics");
 
 const entityRoute = "/";
 const entity = "vets";
 
 //entonces, aqui en el codigo de controller vets, solo se llama a una variable que a su vez
 //llama a listar de los metodos genericos (recordemos que listEntities fue renombrado a list para facilitar su uso)
-const listHandler = list(entity);
+//const listHandler = list(entity);
 
 //y ya cuando queramos crear el enrutador de metodo listar, solo llamamos a la variable anterior y le damos la ruta de entidad
-//correspondiente, en este caso, veterinarias (vets)
-//listar veterinarias
-router.get(entityRoute, listHandler);
+//correspondiente, en este caso, veterinarios (vets)
+//listar veterinarios
+router.get(entityRoute, async (req, res) => {
+  try {
+    const vets = await Vet.find();
+    return res.status(200).json(vets);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ mensaje: error.message });
+  }
+});
 
-//obtener una sola mascota sigue el mismo metodo que en listar todas las veterinarias (anterior)
-const getOneHandler = getOne(entity);
-router.get(`${entityRoute}:_id`, getOneHandler);
+//obtener un solo veterinario sigue el mismo metodo que en listar todos los dueños (anterior)
+//const getOneHandler = getOne(entity);
+router.get(`${entityRoute}:_id`, async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const vets = await Vet.findById(_id);
+    if (vets) {
+      return res.status(200).json(vets);
+    }
+    return res.status(404).json({ mensaje: "vet not found" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ mensaje: error.message });
+  }
+});
 
-//crear veterinarias
-const createHandler = create(entity);
-router.post(entityRoute, createHandler);
+//crear veterinarios
+//const createHandler = create(entity);
+router.post(entityRoute, async (req, res) => {
+  try {
+    const vet = new Vet(req.body);
+    await vet.save();
+    return res.status(200).json(vet);
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).json({ mensaje: error.message });
+  }
+});
 
-//editar veterinarias
-const updateHandler = update(entity);
-router.put(`${entityRoute}:_id`, updateHandler);
+//editar veterinarios
+//const updateHandler = update(entity);
+router.put(`${entityRoute}:_id`, async (req, res) => {
+  try {
+    const { _id = null } = req.params;
+    const { _id: id, ...newData } = req.body;
+    if (!_id) {
+      return res.status(400).json({ mensaje: "missing id" });
+    }
+    //$set es un operador de mongoose que indica setear algo
+    const updatedVet = await Vet.findOneAndUpdate(
+      { _id },
+      { $set: newData },
+      { new: true, runValidators: true }
+    );
+    return res.status(200).json(updatedVet);
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).json({ mensaje: error.message });
+  }
+});
 
-//eliminar veterinarias
-const eraseHandler = erase(entity);
-router.delete(`${entityRoute}:_id`, eraseHandler);
+//eliminar veterinarios
+router.delete(`${entityRoute}:_id`, async (req, res) => {
+  try {
+    const { _id = null } = req.params;
+    if (!_id) {
+      return res.status(400).json({ mensaje: "missing id" });
+    }
+    //$set es un operador de mongoose que indica setear algo
+    const erasedVet = await Vet.findByIdAndDelete({ _id });
+    return res.status(204).json({ mensaje: "vet erased" });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).json({ mensaje: error.message });
+  }
+});
 
 module.exports = router;
---
+++ routes/controllers/vets/schema.js
@@ -0,0 +1,26 @@
+//los schemas de mongoose necesitan la libreria de mongoose
+const mongoose = require("mongoose");
+//vamos a obtener una clase que se llama schema desde mongoose
+const { Schema } = mongoose;
+
+//declarar un nuevo objeto de la clase schema
+const vetSchema = new Schema({
+  vetName: {
+    type: String,
+    required: true,
+  },
+  vetLName: {
+    type: String,
+    required: true,
+  },
+  vetLicense: {
+    type: String,
+    required: true,
+  },
+  vetCountry: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("vets", vetSchema);
